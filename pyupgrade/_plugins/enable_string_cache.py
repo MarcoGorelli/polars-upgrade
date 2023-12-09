bbe3143bcc153c87b0565246320d5cc88a641493
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-12-09 02:47:42 +0000

    add disable/enable string cache

---
+++ pyupgrade/_plugins/enable_string_cache.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import ast
+import functools
+from typing import Iterable
+
+from tokenize_rt import Offset, Token
+
+from pyupgrade._ast_helpers import ast_to_offset
+from pyupgrade._data import register
+from pyupgrade._data import State
+from pyupgrade._data import TokenFunc
+from pyupgrade._ast_helpers import ast_to_offset
+from pyupgrade._ast_helpers import is_name_attr
+from pyupgrade._data import register
+from pyupgrade._data import State
+from pyupgrade._data import TokenFunc
+from pyupgrade._token_helpers import delete_argument
+from pyupgrade._token_helpers import find_op
+from pyupgrade._token_helpers import parse_call_args
+from pyupgrade._token_helpers import replace_argument
+
+def rewrite_to_enable(
+    i: int,
+    tokens: list[Token],
+) -> None:
+    j = find_op(tokens, i, '(')
+    func_args, _ = parse_call_args(tokens, j)
+    i = func_args[0][0]
+    tokens[i] = tokens[i]._replace(src='')
+
+def rewrite_to_disable(
+    i: int,
+    tokens: list[Token],
+) -> None:
+    j = find_op(tokens, i, '(')
+    func_args, _ = parse_call_args(tokens, j)
+    breakpoint()
+    while not (tokens[i].name == 'NAME' and tokens[i].src == 'enable_string_cache'):
+        i += 1
+    breakpoint()
+    tokens[i] = tokens[i]._replace(src='disable_string_cache')
+    i = func_args[0][0]
+    tokens[i] = tokens[i]._replace(src='')
+
+
+
+@register(ast.Call)
+def visit_Call(
+        state: State,
+        node: ast.Call,
+        parent: ast.AST,
+) -> Iterable[tuple[Offset, TokenFunc]]:
+    if (
+            state.settings.current_version >= (0, 19, 3) and
+            isinstance(node.func, ast.Attribute) and
+            isinstance(node.func.value, ast.Name) and
+            node.func.attr == 'enable_string_cache' and
+            node.func.value.id in state.aliases and
+            len(node.args) == 1 and
+            isinstance(node.args[0], ast.Constant)
+    ):
+        if node.args[0].value is True:
+            func = functools.partial(
+                rewrite_to_enable,
+            )
+            yield ast_to_offset(node), func
+        elif node.args[0].value is False:
+            func = functools.partial(
+                rewrite_to_disable,
+            )
+            yield ast_to_offset(node), func
---
+++ pyupgrade/_plugins/toplevel_functions.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import ast
+import functools
+from typing import Iterable
+
+from tokenize_rt import Offset
+
+from pyupgrade._ast_helpers import ast_to_offset
+from pyupgrade._data import register
+from pyupgrade._data import State
+from pyupgrade._data import TokenFunc
+from pyupgrade._token_helpers import replace_name
+
+RENAMINGS = {
+    'avg': ((0, 18, 12), 'mean'),
+    'map': ((0, 19, 0), 'map_batches'), # 0.19.0
+    'apply': ((0, 19, 0), 'map_groups'),
+    'cumsum': ((0, 19, 14), 'cum_sum'),  # 0.19.14
+    'cumfold': ((0, 19, 14), 'cum_fold'),
+    'cumreduce': ((0, 19, 14), 'cum_reduce'),
+    'cumsum_horizontal': ((0, 19, 14), 'cum_sum_horizontal'),
+}
+
+@register(ast.Attribute)
+def visit_Attribute(
+        state: State,
+        node: ast.Attribute,
+        parent: ast.AST,
+) -> Iterable[tuple[Offset, TokenFunc]]:
+    if (
+            isinstance(node.value, ast.Name) and
+            node.value.id in state.aliases and
+            node.attr in RENAMINGS
+    ):
+        min_version, new_name = RENAMINGS[node.attr]
+        if state.settings.current_version >= min_version:
+            new_attr = f'{node.value.id}.{new_name}'
+            func = functools.partial(replace_name, name=node.attr, new=new_attr)
+            yield ast_to_offset(node), func
