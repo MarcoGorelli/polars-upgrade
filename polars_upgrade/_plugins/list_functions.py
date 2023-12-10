ca72fb726110e2fc86a99a59408f15995ee2a7df
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-12-10 09:52:12 +0000

    list functions

---
+++ polars_upgrade/_plugins/list_functions.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import ast
+import functools
+from typing import Iterable
+
+from tokenize_rt import Offset
+from tokenize_rt import Token
+
+from polars_upgrade._ast_helpers import ast_to_offset
+from polars_upgrade._data import register
+from polars_upgrade._data import State
+from polars_upgrade._data import TokenFunc
+from polars_upgrade._token_helpers import is_simple_expression
+
+
+def rename(
+    i: int,
+    tokens: list[Token],
+    *,
+    name: str,
+    new: str,
+) -> None:
+    while not (tokens[i].name == 'NAME' and tokens[i].src == name):
+        i += 1
+    tokens[i] = tokens[i]._replace(src=new)
+
+
+RENAMINGS = {
+    'count_match': ((0, 19, 3), 'count_matches'),
+    'lengths': ((0, 19, 8), 'len'),
+    'take': ((0, 19, 14), 'gather'),
+}
+
+
+@register(ast.Attribute)
+def visit_Attribute(
+        state: State,
+        node: ast.Attribute,
+        parent: ast.AST,
+) -> Iterable[tuple[Offset, TokenFunc]]:
+    if (
+            isinstance(node.value, ast.Attribute) and
+            is_simple_expression(node.value.value, state.aliases) and
+            node.value.attr == 'list' and
+            node.attr in RENAMINGS
+    ):
+        min_version, new_name = RENAMINGS[node.attr]
+        if state.settings.current_version >= min_version:
+            new_attr = new_name
+            func = functools.partial(
+                rename, name=node.attr,
+                new=new_attr,
+            )
+            yield ast_to_offset(node), func
---
+++ tests/list_functions_test.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import pytest
+
+from polars_upgrade._data import Settings
+from polars_upgrade._main import _fix_plugins
+
+
+@pytest.mark.parametrize(
+    ('s', 'version'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'pl.col("a").list.count_match()\n',
+            (0, 17, 0),
+            id='too old',
+        ),
+    ),
+)
+def test_fix_capture_output_noop(s, version):
+    assert _fix_plugins(s, settings=Settings(current_version=version)) == s
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'pl.col("a").list.count_match()\n',
+            'import polars as pl\n'
+            'pl.col("a").list.count_matches()\n',
+        ),
+    ),
+)
+def test_fix_capture_output(s, expected):
+    ret = _fix_plugins(s, settings=Settings(current_version=(0, 19, 18)))
+    assert ret == expected
