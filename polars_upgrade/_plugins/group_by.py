abadcb3ebdd0b934cc5c8b8df5b69a3e73353f66
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-12-10 08:45:36 +0000

    fix version

---
+++ polars_upgrade/_plugins/group_by.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import ast
+import functools
+from typing import Iterable
+
+from tokenize_rt import Offset
+from tokenize_rt import Token
+
+from polars_upgrade._ast_helpers import ast_to_offset
+from polars_upgrade._data import register
+from polars_upgrade._data import State
+from polars_upgrade._data import TokenFunc
+
+
+def rename(
+    i: int,
+    tokens: list[Token],
+    *,
+    name: str,
+    new: str,
+) -> None:
+    while not (tokens[i].name == 'NAME' and tokens[i].src == name):
+        i += 1
+    tokens[i] = tokens[i]._replace(src=new)
+
+
+RENAMINGS = {
+    'groupby_dynamic': ((0, 19, 0), 'group_by_dynamic'),
+    'groupby_rolling': ((0, 19, 0), 'group_by_rolling'),
+    'groupby': ((0, 19, 0), 'group_by'),
+}
+
+
+@register(ast.Attribute)
+def visit_Attribute(
+        state: State,
+        node: ast.Attribute,
+        parent: ast.AST,
+) -> Iterable[tuple[Offset, TokenFunc]]:
+    if (
+            node.attr in RENAMINGS
+    ):
+        min_version, new_name = RENAMINGS[node.attr]
+        if state.settings.current_version >= min_version:
+            func = functools.partial(rename, name=node.attr, new=new_name)
+            yield ast_to_offset(node), func
---
+++ tests/group_by_dynamic_test.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import pytest
+
+from polars_upgrade._data import Settings
+from polars_upgrade._main import _fix_plugins
+
+
+@pytest.mark.parametrize(
+    ('s', 'version'),
+    (
+        pytest.param(
+            'df.groupby_dynamic',
+            (0, 17, 0),
+            id='too old',
+        ),
+    ),
+)
+def test_fix_capture_output_noop(s, version):
+    assert _fix_plugins(s, settings=Settings(current_version=version)) == s
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        pytest.param(
+            'df.groupby_dynamic',
+            'df.group_by_dynamic',
+        ),
+    ),
+)
+def test_fix_capture_output(s, expected):
+    ret = _fix_plugins(s, settings=Settings(current_version=(0, 19, 16)))
+    assert ret == expected
