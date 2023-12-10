1748d56da4a73625b49cfd4a833db27d37c0461c
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-12-10 08:38:01 +0000

    coverage

---
+++ tests/enable_string_cache_test.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import pytest
+
+from polars_upgrade._data import Settings
+from polars_upgrade._main import _fix_plugins
+
+
+@pytest.mark.parametrize(
+    ('s', 'version'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'pl.enable_string_cache(True)\n',
+            (0, 17, 0),
+        ),
+        pytest.param(
+            'import polars as pl\n'
+            'pl.enable_string_cache(1)\n',
+            (0, 20, 0),
+            id='invalid',
+        ),
+    ),
+)
+def test_fix_capture_output_noop(s, version):
+    assert _fix_plugins(s, settings=Settings(current_version=version)) == s
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'pl.enable_string_cache(True)\n',
+            'import polars as pl\n'
+            'pl.enable_string_cache()\n',
+        ),
+        pytest.param(
+            'import polars as pl\n'
+            'pl.enable_string_cache(False)\n',
+            'import polars as pl\n'
+            'pl.disable_string_cache()\n',
+        ),
+    ),
+)
+def test_fix_capture_output(s, expected):
+    ret = _fix_plugins(s, settings=Settings(current_version=(0, 19, 19)))
+    assert ret == expected
