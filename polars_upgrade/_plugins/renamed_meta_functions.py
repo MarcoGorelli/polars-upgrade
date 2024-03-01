e6d4544c99d8737cf7a7a7c7003b133b85bd7070
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-01 17:54:10 +0000

    add meta renaming

---
+++ polars_upgrade/_plugins/renamed_meta_functions.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import ast
+import functools
+from typing import Iterable
+
+from tokenize_rt import Offset
+from tokenize_rt import Token
+
+from polars_upgrade._ast_helpers import ast_to_offset
+from polars_upgrade._data import register
+from polars_upgrade._data import State
+from polars_upgrade._data import TokenFunc
+from polars_upgrade._token_helpers import is_simple_expression
+
+
+def rename(
+    i: int,
+    tokens: list[Token],
+    *,
+    name: str,
+    new: str,
+) -> None:
+    while not (tokens[i].name == 'NAME' and tokens[i].src == name):
+        i += 1
+    tokens[i] = tokens[i]._replace(src=new)
+
+
+RENAMINGS = {
+    'write_json': ((0, 20, 11), 'write_json'),
+}
+
+
+@register(ast.Attribute)
+def visit_Attribute(
+        state: State,
+        node: ast.Attribute,
+        parent: ast.AST,
+) -> Iterable[tuple[Offset, TokenFunc]]:
+    if (
+            isinstance(node.value, ast.Attribute) and
+            is_simple_expression(node.value.value, state.aliases) and
+            node.value.attr == 'list' and
+            node.attr in RENAMINGS
+    ):
+        min_version, new_name = RENAMINGS[node.attr]
+        if state.settings.target_version >= min_version:
+            new_attr = new_name
+            func = functools.partial(
+                rename, name=node.attr,
+                new=new_attr,
+            )
+            yield ast_to_offset(node), func
---
+++ tests/renamed_meta_functions.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import pytest
+
+from polars_upgrade._data import Settings
+from polars_upgrade._main import _fix_plugins
+
+
+@pytest.mark.parametrize(
+    ('s', 'version'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'pl.col("a").meta.write_json\n',
+            (0, 20, 10),
+            id='too old',
+        ),
+    ),
+)
+def test_fix_capture_output_noop(s, version):
+    assert _fix_plugins(s, settings=Settings(target_version=version)) == s
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'pl.col("a").meta.write_json(file)\n',
+            'import polars as pl\n'
+            'pl.col("a").meta.serialize(file)\n',
+        ),
+    ),
+)
+def test_fix_capture_output(s, expected):
+    ret = _fix_plugins(s, settings=Settings(target_version=(0, 20, 11)))
+    assert ret == expected
