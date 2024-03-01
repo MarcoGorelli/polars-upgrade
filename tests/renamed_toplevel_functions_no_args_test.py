a7eb08f21cf29c7634519257b0169a9452933397
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-01 19:02:32 +0000

    add test

---
+++ tests/renamed_toplevel_functions_no_args_test.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import pytest
+
+from polars_upgrade._data import Settings
+from polars_upgrade._main import _fix_plugins
+
+
+@pytest.mark.parametrize(
+    ('s', 'version'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'pl.count()\n',
+            (0, 17, 0),
+            id='too old',
+        ),
+        pytest.param(
+            'import polars as pl\n'
+            'pl.count(a)\n',
+            (0, 20, 5),
+            id='not deprecated',
+        ),
+    ),
+)
+def test_fix_capture_output_noop(s, version):
+    assert _fix_plugins(s, settings=Settings(target_version=version)) == s
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'pl.count()\n',
+            'import polars as pl\n'
+            'pl.len()\n',
+            id='yeah',
+        ),
+    ),
+)
+def test_fix_capture_output(s, expected):
+    ret = _fix_plugins(s, settings=Settings(target_version=(0, 20, 4)))
+    assert ret == expected
