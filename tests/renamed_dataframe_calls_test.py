6dd5745af4aabeb488de9eaa867888ba1a9dc778
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-02 10:03:56 +0000

    approx n unique

---
+++ tests/renamed_dataframe_calls_test.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import pytest
+
+from polars_upgrade._data import Settings
+from polars_upgrade._main import _fix_plugins
+
+
+@pytest.mark.parametrize(
+    ('s', 'version'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'df.approx_n_unique()\n',
+            (0, 19, 19),
+            id='too old',
+        ),
+    ),
+)
+def test_fix_capture_output_noop(s, version):
+    assert _fix_plugins(s, settings=Settings(target_version=version)) == s
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        pytest.param(
+            'import polars as pl\n'
+            'df.approx_n_unique()\n',
+            'import polars as pl\n'
+            'df.select(pl.all().approx_n_unique())\n',
+        ),
+    ),
+)
+def test_fix_capture_output(s, expected):
+    ret = _fix_plugins(s, settings=Settings(target_version=(0, 20, 11)))
+    assert ret == expected
