44c9a24616ef49079b6b92125a6cafd48bbd21ae
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-06 15:59:42 +0000

    Bump version to 0.3.1

--- polars_upgrade/__init__.py
+++ polars_upgrade/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from polars_upgrade._main import fix_plugins as rewrite
 from polars_upgrade._main import Settings
