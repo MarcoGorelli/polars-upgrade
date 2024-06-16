e5acfc8ffff2540be9e042bb244ca22f9571a5c9
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-06-16 09:42:57 +0100

    much better test

--- polars_upgrade/_plugins/removed_expr_arguments.py
+++ polars_upgrade/_plugins/removed_expr_arguments.py
@@ -12,6 +12,7 @@ from polars_upgrade._data import register
 from polars_upgrade._data import State
 from polars_upgrade._data import TokenFunc
 from polars_upgrade._token_helpers import delete_argument
+from polars_upgrade._token_helpers import find_op
 from polars_upgrade._token_helpers import is_simple_expression
 from polars_upgrade._token_helpers import parse_call_args
 
@@ -20,18 +21,14 @@ def remove_argument(
     i: int,
     tokens: list[Token],
     *,
-    line: int,
-    offset: int,
+    function_name: str,
     arg_idxs: list[int],
 ) -> None:
-    # go forwards to first arg
-    while ((tokens[i].line, tokens[i].utf8_byte_offset) < (line, offset)):
+    # go forwards to function name
+    while not (tokens[i].name == 'NAME' and tokens[i].src == function_name):
         i += 1
-    # go backwards to opening paren
-    i -= 1
-    while not (tokens[i].name == 'OP' and tokens[i].src == '('):
-        i -= 1
-    func_args, _ = parse_call_args(tokens, i)
+    j = find_op(tokens, i, '(')
+    func_args, _ = parse_call_args(tokens, j)
     for idx in reversed(arg_idxs):
         delete_argument(idx, tokens, func_args)
 
@@ -59,18 +56,16 @@ def visit_Attribute(
         )
     ):
         min_version, args = DELETIONS[parent.func.attr]
-        lineno, col_offset = min(
-            (kwarg.lineno, kwarg.col_offset) for kwarg in [*parent.keywords, *parent.args]
-        )
         idxs = []
         for idx, kwarg in enumerate(parent.keywords, start=len(parent.args)):
             if kwarg.arg in args:
                 idxs.append(idx)
+        if not idxs:
+            return
         if state.settings.target_version >= min_version:
             func = functools.partial(
                 remove_argument,
-                line=lineno,
-                offset=col_offset,
+                function_name=parent.func.attr,
                 arg_idxs=idxs,
             )
             yield ast_to_offset(parent.func), func
